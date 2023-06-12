c224e77cfce4f1eb678741ac13caa03766b608d6
Zach Sanford <sanfordzachary@gmail.com>
2023-06-12 11:07:48 -0700

    Project initialized.

---
+++ src/NOAA.NET/Class1.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="Class1.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+namespace NOAA.NET;
+
+/// <summary>
+/// Documented.
+/// </summary>
+public class Class1
+{
+}
