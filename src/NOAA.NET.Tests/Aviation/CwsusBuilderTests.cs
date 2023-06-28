22b055b816193df56cb9dfbeaf951ad32226d64b
Zach Sanford <sanfordzachary@gmail.com>
2023-06-28 15:56:09 -0700

    Added CwsusBuilderTests.cs to the unit test project.

---
+++ src/NOAA.NET.Tests/Aviation/CwsusBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using NOAA.NET.Aviation;
+using NOAA.NET.Extensions;
+using NOAA.NET.Aviation.Types;
+
+namespace NOAA.NET.Tests.Aviation;
+
+public class CwsusBuilderTests
+{
+    [Fact]
+    public void Builder_Working_Constructor()
+    {
+        // Arrange
+        CwsusBuilder? builder = null;
+        CwsuId expectedId = CwsuId.ZSE;
+
+        // Act
+        builder = new()
+        {
+            CwsuId = CwsuId.ZSE
+        };
+
+        // Assert
+        Assert.Equal(expectedId, builder.CwsuId);
+    }
+}
