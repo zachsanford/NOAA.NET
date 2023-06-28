c2d8bd2de824ad534c957f18c6e59f0678f46cb2
Zach Sanford <sanfordzachary@gmail.com>
2023-06-28 14:32:05 -0700

    Added the CwsuBuilder Class.

---
+++ src/NOAA.NET/Aviation/CwsuBuilder.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="CwsuBuilder.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using NOAA.NET.Aviation.Types;
+
+namespace NOAA.NET.Aviation;
+
+/// <summary>
+/// Builder Class for the Aviation/CWSUS Endpoint.
+/// </summary>
+public class CwsuBuilder
+{
+    /// <summary>
+    /// Gets or sets the NWS CWSU ID Property.
+    /// </summary>
+    public CwsuId CwsuId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Date Property.
+    /// </summary>
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+    public DateTime? Date { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Sequence Number Property.
+    /// </summary>
+    public int? Sequence { get; set; }
+}
