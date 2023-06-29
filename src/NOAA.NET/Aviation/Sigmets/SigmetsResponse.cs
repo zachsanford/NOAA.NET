8ee344c70b941aa3186587df85bfa428c40a46aa
Zach Sanford <sanfordzachary@gmail.com>
2023-06-29 11:36:22 -0700

    Added the SigmetsResponse class and subclasses.

---
+++ src/NOAA.NET/Aviation/Sigmets/SigmetsResponse.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="SigmetsResponse.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+namespace NOAA.NET.Aviation.Sigmets;
+
+/// <summary>
+/// A representation of an Aviation/Sigmets Endpoint
+/// response.
+/// </summary>
+public class SigmetsResponse
+{
+    /// <summary>
+    /// Gets or sets the <see cref="Context"/> Property.
+    /// </summary>
+    public Context? Context { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Type Property.
+    /// </summary>
+    public string? Type { get; set; }
+
+    /// <summary>
+    /// Gets or sets a collection of <see cref="Feature"/> objects.
+    /// </summary>
+    public Feature[]? Features { get; set; }
+}
