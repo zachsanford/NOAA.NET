21eaf337740ee626d2ed6f98504da3309a409474
Zach Sanford <sanfordzachary@gmail.com>
2023-06-30 04:46:29 -0700

    Added SigmetsBuilderTests.cs to the unit test project.

---
+++ src/NOAA.NET.Tests/Aviation/Sigmets/SigmetsBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿using NOAA.NET.Aviation.Sigmets;
+
+namespace NOAA.NET.Tests.Aviation.Sigmets;
+
+public class SigmetsBuilderTests
+{
+    [Fact]
+    public void Builder_Working_Constructor()
+    {
+        // Arrange
+        SigmetsBuilder? builder = null;
+        string expectedAtsu = "MMMM";
+
+        // Act
+        builder = new()
+        {
+            Atsu = "MMMM"
+        };
+
+        // Assert
+        Assert.Equal(expectedAtsu, builder.Atsu);
+    }
+}
