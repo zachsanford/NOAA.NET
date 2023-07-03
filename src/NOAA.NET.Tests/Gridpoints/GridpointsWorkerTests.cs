a7d66cece1f68abde3ff24d0bdbd2506cb8b124f
Zach Sanford <sanfordzachary@gmail.com>
2023-07-03 15:27:36 -0700

    Added the GridpointsWorkerTests class to the unit test project.

---
+++ src/NOAA.NET.Tests/Gridpoints/GridpointsWorkerTests.cs
@@ -0,0 +1,30 @@
+﻿using NOAA.NET.Gridpoints;
+using NOAA.NET.Gridpoints.Types;
+
+namespace NOAA.NET.Tests.Gridpoints;
+
+public class GridpointsWorkerTests
+{
+    [Fact]
+    public async void Worker_Working_Constructor()
+    {
+        // Arrange
+        GridpointsBuilder? builder = null;
+        GridpointsWorker? worker = null;
+        GridpointsResponse? response = null;
+
+        // Act
+        builder = new()
+        {
+            Wfo = WFO.MFR,
+            XCoordinate = 100,
+            YCoordinate = 100
+        };
+
+        worker = new(builder);
+        response = await worker.CallEndpointAsync();
+
+        // Assert
+        Assert.NotNull(response.Properties);
+    }
+}
