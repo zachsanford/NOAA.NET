c4269feeeeb78a119ab2a437592dc3c63c03d348
Zach Sanford <sanfordzachary@gmail.com>
2023-07-03 14:31:19 -0700

    Added the GridpointsClient class.

---
+++ src/NOAA.NET/Services/Network/GridpointsClient.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="GridpointsClient.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using NOAA.NET.Gridpoints;
+
+namespace NOAA.NET.Services.Network;
+
+/// <summary>
+/// Http client for the Gridpoints endpoint of the API.
+/// </summary>
+internal sealed class GridpointsClient : NOAAClient<GridpointsResponse>
+{
+    /// <inheritdoc/>
+    protected override string BaseURL { get; } = @"https://api.weather.gov/gridpoints";
+
+    /// <inheritdoc/>
+    protected override string EndpointType { get; } = "Gridpoints";
+}
