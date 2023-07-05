60e36e77845a1976bb3fce83f7dd6f04c66be675
Zach Sanford <sanfordzachary@gmail.com>
2023-07-05 14:48:27 -0700

    Added the GridpointsForecastWorker class.

--- src/NOAA.NET/Gridpoints/Forecast/GridpointsForecastWorker.cs
+++ src/NOAA.NET/Gridpoints/Forecast/GridpointsForecastWorker.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="GridpointsBaseWorker.cs" company="Zach Sanford">
+﻿// <copyright file="GridpointsForecastWorker.cs" company="Zach Sanford">
 // Copyright (c) Zach Sanford. All rights reserved.
 // </copyright>
 
@@ -9,24 +9,25 @@ using NOAA.NET.Extensions;
 using NOAA.NET.Interfaces;
 using NOAA.NET.Services.Network;
 
-namespace NOAA.NET.Gridpoints.Base;
+namespace NOAA.NET.Gridpoints.Forecast;
 
 /// <summary>
-/// Public worker for the API's Gridpoints Endpoint.
+/// Public worker for the API's Gridpoints Forecast
+/// Endpoint.
 /// </summary>
-public sealed class GridpointsBaseWorker : IWorker<GridpointsBaseResponse>
+public sealed class GridpointsForecastWorker : IWorker<GridpointsForecastResponse>
 {
-    private readonly GridpointsBaseBuilder _builder;
+    private readonly GridpointsForecastBuilder _builder;
     private string? _endpointPropertiesURL;
-    private GridpointsBaseClient _client = new();
+    private GridpointsForecastClient _client = new();
     private StringBuilder _stringBuilder = new("/");
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="GridpointsBaseWorker"/> class.
+    /// Initializes a new instance of the <see cref="GridpointsForecastWorker"/> class.
     /// </summary>
-    /// <param name="builder">Required <see cref="GridpointsBaseBuilder"/> object.</param>
+    /// <param name="builder">Required <see cref="GridpointsForecastBuilder"/> object.</param>
     /// <exception cref="ArgumentNullException">NULL Exception.</exception>
-    public GridpointsBaseWorker(GridpointsBaseBuilder builder)
+    public GridpointsForecastWorker(GridpointsForecastBuilder builder)
     {
         if (builder == null)
         {
@@ -38,6 +39,14 @@ public sealed class GridpointsBaseWorker : IWorker<GridpointsBaseResponse>
             this._stringBuilder.Append(builder.Wfo.GetStringValue() + '/');
             this._stringBuilder.Append(builder.XCoordinate.ToString() + ',');
             this._stringBuilder.Append(builder.YCoordinate.ToString());
+            this._stringBuilder.Append("/forecast");
+
+            if (builder.Units != null)
+            {
+                this._stringBuilder.Append("?units=");
+                this._stringBuilder.Append(builder.Units.GetStringValue());
+            }
+
             this._endpointPropertiesURL = this._stringBuilder.ToString();
             this._client.EndpointURL = this._endpointPropertiesURL;
         }
@@ -46,11 +55,11 @@ public sealed class GridpointsBaseWorker : IWorker<GridpointsBaseResponse>
     /// <summary>
     /// Calls the API's Gridpoints Endpoint.
     /// </summary>
-    /// <returns><see cref="GridpointsBaseResponse"/> payload.</returns>
+    /// <returns><see cref="GridpointsForecastResponse"/> payload.</returns>
     /// <exception cref="Exception">NULL exception and Wrong Gridpoints Exception.</exception>
-    public async Task<GridpointsBaseResponse> CallEndpointAsync()
+    public async Task<GridpointsForecastResponse> CallEndpointAsync()
     {
-        GridpointsBaseResponse? gridpointsResponse;
+        GridpointsForecastResponse? gridpointsResponse;
 
         try
         {
@@ -70,7 +79,7 @@ public sealed class GridpointsBaseWorker : IWorker<GridpointsBaseResponse>
         }
         else
         {
-            throw new Exception(message: "Called the Gridpoints API Endpoint. Return payload is NULL.");
+            throw new Exception(message: "Called the Gridpoints Forecast API Endpoint. Return payload is NULL.");
         }
     }
 }
