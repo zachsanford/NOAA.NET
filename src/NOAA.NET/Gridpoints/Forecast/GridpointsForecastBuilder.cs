0010024ae588b3cee9aafd2a6fd1eb2696755cd0
Zach Sanford <sanfordzachary@gmail.com>
2023-07-05 14:26:20 -0700

    Added the GridpointsForecastBuilder class.

--- src/NOAA.NET/Gridpoints/Forecast/GridpointsForecastBuilder.cs
+++ src/NOAA.NET/Gridpoints/Forecast/GridpointsForecastBuilder.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Zach Sanford. All rights reserved.
 // </copyright>
 
+using NOAA.NET.Gridpoints.Types;
+
 namespace NOAA.NET.Gridpoints.Forecast;
 
 /// <summary>
@@ -10,4 +12,27 @@ namespace NOAA.NET.Gridpoints.Forecast;
 /// </summary>
 public sealed class GridpointsForecastBuilder
 {
+    /// <summary>
+    /// Gets or sets the Units in textual output Property.
+    /// Defaults to US if left unspecified.
+    /// </summary>
+    public Units? Units { get; set; } = Types.Units.US;
+
+    /// <summary>
+    /// Gets or sets the <see cref="WFO"/> type, for the
+    /// Weather Forecast Office Property.
+    /// </summary>
+    public required WFO Wfo { get; set; }
+
+    /// <summary>
+    /// Gets or sets the forecast grid X coordinate
+    /// Property.
+    /// </summary>
+    public required byte XCoordinate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the forecast grid Y coordinate
+    /// Property.
+    /// </summary>
+    public required byte YCoordinate { get; set; }
 }
