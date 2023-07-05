37fd59beb9d86e8f3ac47b05f7b2bbc450487960
Zach Sanford <sanfordzachary@gmail.com>
2023-07-05 14:19:21 -0700

    Added the Units enum class.

---
+++ src/NOAA.NET/Gridpoints/Forecast/GridpointsForecastBuilder.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="GridpointsForecastBuilder.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+namespace NOAA.NET.Gridpoints.Forecast;
+
+/// <summary>
+/// Builder class for the Gridpoints Forecast
+/// Endpoint.
+/// </summary>
+public sealed class GridpointsForecastBuilder
+{
+}
---
+++ src/NOAA.NET/Gridpoints/Types/Units.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Units.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using NOAA.NET.Extensions;
+
+namespace NOAA.NET.Gridpoints.Types;
+
+/// <summary>
+/// Units enum for the Gridpoints Forecast
+/// Endpoint Units Parameter.
+/// </summary>
+public enum Units
+{
+    /// <summary>
+    /// US type for the "Units" value of the Gridpoints Forecast
+    /// Endpoint.
+    /// </summary>
+    [StringValue("us")]
+    US,
+
+    /// <summary>
+    /// SI type for the "Units" value of the Gridpoints Forecast
+    /// Endpoint.
+    /// </summary>
+    [StringValue("si")]
+    SI
+}
