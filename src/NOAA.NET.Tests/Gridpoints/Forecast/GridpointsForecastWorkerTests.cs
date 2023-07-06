3b05fc6cfd0372acfa413a7be42c20858518b09f
Zach Sanford <sanfordzachary@gmail.com>
2023-07-06 12:18:24 -0700

    Added the GridpointsForecastWorkerTests class to the unit test project.

---
+++ src/NOAA.NET.Tests/Gridpoints/Forecast/GridpointsForecastWorkerTests.cs
@@ -0,0 +1,38 @@
+﻿using NOAA.NET.Gridpoints.Forecast;
+using NOAA.NET.Gridpoints.Types;
+
+namespace NOAA.NET.Tests.Gridpoints.Forecast;
+
+public class GridpointsForecastWorkerTests
+{
+    [Theory]
+    [InlineData(null, WFO.MFR, 98, 116)]
+    [InlineData(Units.SI, WFO.MFR, 98, 116)]
+    public async void Worker_Working_Constructor(Units? unitsParameter,
+        WFO wfoParameter, byte xParameter, byte yParameter)
+    {
+        // Arrange
+        GridpointsForecastBuilder? builder = null;
+        GridpointsForecastWorker? worker = null;
+        GridpointsForecastResponse? response = null;
+
+        // Act
+        builder = new()
+        {
+            Wfo = wfoParameter,
+            XCoordinate = xParameter,
+            YCoordinate = yParameter
+        };
+
+        if (unitsParameter != null)
+        {
+            builder.Units = unitsParameter;
+        }
+
+        worker = new(builder);
+        response = await worker.CallEndpointAsync();
+
+        // Assert
+        Assert.NotNull(response.Geometry);
+    }
+}
