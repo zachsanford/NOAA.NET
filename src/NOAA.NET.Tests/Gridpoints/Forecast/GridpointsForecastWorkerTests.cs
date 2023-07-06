8f33a3c7ad583739570e9fc8329b5b4776870fa5
Zach Sanford <sanfordzachary@gmail.com>
2023-07-06 15:05:18 -0700

    Added the Worker_Working_Constructor_Hourly method to the GridpointsForecastWorkerTests class in the unit test project.

--- src/NOAA.NET.Tests/Gridpoints/Forecast/GridpointsForecastWorkerTests.cs
+++ src/NOAA.NET.Tests/Gridpoints/Forecast/GridpointsForecastWorkerTests.cs
@@ -35,4 +35,37 @@ public class GridpointsForecastWorkerTests
         // Assert
         Assert.NotNull(response.Geometry);
     }
+
+    [Theory]
+    [InlineData(null, true, WFO.MFR, 98, 116)]
+    [InlineData(Units.SI, true, WFO.MFR, 98, 116)]
+    [InlineData(Units.US, false, WFO.MFR, 98, 116)]
+    public async void Worker_Working_Constructor_Hourly(Units? unitsParameter,
+        bool isHourlyParameter, WFO wfoParameter, byte xParameter, byte yParameter)
+    {
+        // Arrange
+        GridpointsForecastBuilder? builder = null;
+        GridpointsForecastWorker? worker = null;
+        GridpointsForecastResponse? response = null;
+
+        // Act
+        builder = new()
+        {
+            IsHourly = isHourlyParameter,
+            Wfo = wfoParameter,
+            XCoordinate = xParameter,
+            YCoordinate = yParameter
+        };
+
+        if (unitsParameter != null)
+        {
+            builder.Units = unitsParameter;
+        }
+
+        worker = new(builder);
+        response = await worker.CallEndpointAsync();
+
+        // Assert
+        Assert.NotNull(response.Geometry);
+    }
 }
