a748711f875fbf1d3a2c7c4b818e7ed8207114d7
Zach Sanford <sanfordzachary@gmail.com>
2023-07-10 15:19:16 -0700

    Added the StationsClient class.

---
+++ src/NOAA.NET/Services/Network/StationsClient.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="StationsClient.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using NOAA.NET.Stations;
+
+namespace NOAA.NET.Services.Network;
+
+/// <summary>
+/// Http client for the Stations endpoint of the API.
+/// </summary>
+internal sealed class StationsClient : NOAAClient<StationsResponse>
+{
+    /// <inheritdoc/>
+    protected override string BaseURL { get; } = @"https://api.weather.gov/stations";
+
+    /// <inheritdoc/>
+    protected override string EndpointType { get; } = "Stations";
+}
