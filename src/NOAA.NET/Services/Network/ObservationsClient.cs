3d796f89a5784cf3979308fc5bb7d9c9c4983b78
Zach Sanford <sanfordzachary@gmail.com>
2023-07-13 12:25:46 -0700

    Added the ObservationsClient class.

---
+++ src/NOAA.NET/Services/Network/ObservationsClient.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ObservationsClient.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using NOAA.NET.Stations.Observations;
+
+namespace NOAA.NET.Services.Network;
+
+/// <summary>
+/// Http client for the Stations\Observations endpoint of the API.
+/// </summary>
+internal sealed class ObservationsClient : NOAAClient<ObservationsResponse>
+{
+    /// <inheritdoc/>
+    protected override string BaseURL { get; } = @"https://api.weather.gov/stations/";
+
+    /// <inheritdoc/>
+    protected override string EndpointType { get; } = "Stations Observations";
+}
