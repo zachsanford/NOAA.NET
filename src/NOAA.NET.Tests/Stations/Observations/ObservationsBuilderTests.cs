a0ce07c73069a879b06b68c4b670b54e75e531f7
Zach Sanford <sanfordzachary@gmail.com>
2023-07-13 13:48:30 -0700

    Added the ObservationsBuilderTests class to the unit test project.

---
+++ src/NOAA.NET.Tests/Stations/Observations/ObservationsBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using NOAA.NET.Stations.Observations;
+
+namespace NOAA.NET.Tests.Stations.Observations;
+
+public class ObservationsBuilderTests
+{
+    [Fact]
+    public void Builder_Working_Constructor()
+    {
+        // Arrange
+        ObservationsBuilder? builder = null;
+
+        // Act
+        builder = new()
+        {
+            StationId = "KRBG"
+        };
+
+        // Assert
+        Assert.NotNull(builder);
+    }
+}
