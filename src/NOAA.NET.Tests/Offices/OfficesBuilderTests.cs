ea30a42cf2d487f4b9e68473586ab0ba3c4beee1
Zach Sanford <sanfordzachary@gmail.com>
2023-07-18 15:29:32 -0700

    Added the OfficesBuilderTests class to the unit test project.

---
+++ src/NOAA.NET.Tests/Offices/OfficesBuilderTests.cs
@@ -0,0 +1,24 @@
+﻿using NOAA.NET.Offices;
+using NOAA.NET.Types;
+
+namespace NOAA.NET.Tests.Offices;
+
+public class OfficesBuilderTests
+{
+    [Fact]
+    public void Builder_Working_Constructor()
+    {
+        // Arrange
+        OfficesBuilder? builder = null;
+        WFO expectedOfficeId = WFO.MFR;
+
+        // Act
+        builder = new()
+        {
+            OfficeId = WFO.MFR
+        };
+
+        // Assert
+        Assert.Equal(expectedOfficeId, builder.OfficeId);
+    }
+}
