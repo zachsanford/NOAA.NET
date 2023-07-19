f4c0a7ccc50d9db169b23896ec992e6727b7f61d
Zach Sanford <sanfordzachary@gmail.com>
2023-07-19 11:47:28 -0700

    Added the OfficesHeadlinesClient class.

---
+++ src/NOAA.NET/Services/Network/OfficesHeadlinesClient.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="OfficesHeadlinesClient.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using NOAA.NET.Offices;
+
+namespace NOAA.NET.Services.Network;
+
+/// <summary>
+/// Http client for the Offices/Headlines endpoint of the
+/// API.
+/// </summary>
+internal sealed class OfficesHeadlinesClient : NOAAClient<OfficesHeadlinesResponse>
+{
+    /// <inheritdoc/>
+    protected override string BaseURL { get; } = @"https://api.weather.gov/offices";
+
+    /// <inheritdoc/>
+    protected override string EndpointType { get; } = "Offices Headlines";
+}
