2c06b19e3dc80370877301341e87cff158e819f5
Zach Sanford <sanfordzachary@gmail.com>
2023-07-19 11:40:39 -0700

    Added the OfficesHeadlinesResponse class and subclasses.

---
+++ src/NOAA.NET/Offices/Graph.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="Graph.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using System;
+using System.Text.Json.Serialization;
+
+namespace NOAA.NET.Offices;
+
+/// <summary>
+/// A representation of an OfficeHeadline
+/// object.
+/// </summary>
+public class Graph
+{
+    /// <summary>
+    /// Gets or sets the @Id URL property.
+    /// </summary>
+    [JsonPropertyName("@id")]
+    public string? MainId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Id property.
+    /// </summary>
+    public string? Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Office
+    /// URL property.
+    /// </summary>
+    public string? Office { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Important flag
+    /// property.
+    /// </summary>
+    public bool? Important { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Issuance Time
+    /// property.
+    /// </summary>
+    public DateTime? IssuanceTime { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Link URL property.
+    /// </summary>
+    public string? Link { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Name property.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Title property.
+    /// </summary>
+    public string? Title { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Summary property.
+    /// </summary>
+    public string? Summary { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Content property.
+    /// </summary>
+    public string? Content { get; set; }
+}
---
+++ src/NOAA.NET/Offices/OfficesHeadlinesResponse.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="OfficesHeadlinesResponse.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+namespace NOAA.NET.Offices;
+
+/// <summary>
+/// A representation of an OfficeHeadlineCollection
+/// object.
+/// </summary>
+public class OfficesHeadlinesResponse
+{
+    /// <summary>
+    /// Gets or sets the
+    /// <see cref="NOAA.NET.Offices.Context"/>
+    /// property.
+    /// </summary>
+    public Context? Context { get; set; }
+
+    /// <summary>
+    /// Gets or sets the
+    /// <see cref="NOAA.NET.Offices.Graph"/>
+    /// collection property.
+    /// </summary>
+    public Graph[]? Graph { get; set; }
+}
