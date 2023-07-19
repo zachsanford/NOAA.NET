7e19cd94af60334fb745cc0bb142bf227d1a8bf4
Zach Sanford <sanfordzachary@gmail.com>
2023-07-19 14:54:57 -0700

    Added the PointsClient class.

---
+++ src/NOAA.NET/Services/Network/PointsClient.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="PointsClient.cs" company="Zach Sanford">
+// Copyright (c) Zach Sanford. All rights reserved.
+// </copyright>
+
+using NOAA.NET.Points;
+
+namespace NOAA.NET.Services.Network;
+
+/// <summary>
+/// Http client for the Points endpoint of the API.
+/// </summary>
+internal sealed class PointsClient : NOAAClient<PointsResponse>
+{
+    /// <inheritdoc/>
+    protected override string BaseURL { get; } = @"https://api.weather.gov/points";
+
+    /// <inheritdoc/>
+    protected override string EndpointType { get; } = "Points";
+}
