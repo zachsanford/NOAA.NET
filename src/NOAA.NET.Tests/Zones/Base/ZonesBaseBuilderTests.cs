7126751651046398faa876b1d95b9fb2bfa729a7
Zach Sanford <sanfordzachary@gmail.com>
2023-08-09 12:57:00 -0700

    Backing up for the day. Need to make a ZonesBaseWorker class and then finish the ZonesBase Unit Tests.

--- src/NOAA.NET.Tests/Zones/Base/ZonesBaseBuilderTests.cs
+++ src/NOAA.NET.Tests/Zones/Base/ZonesBaseBuilderTests.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NOAA.NET.Zones.Base;
 
-namespace NOAA.NET.Tests.Zones.Base
+namespace NOAA.NET.Tests.Zones.Base;
+
+public class ZonesBaseBuilderTests
 {
-    internal class ZonesBaseBuilderTests
+    [Fact]
+    public void Builder_Working_Constructor()
     {
+        // Arrange
+        ZonesBaseBuilder? builder = null;
+        string expectedPoint = "43.2165,-123.34174";
+
+        // Act
+        builder = new()
+        {
+            Point = "43.2165,-123.34174"
+        };
+
+        // Assert
+        Assert.Equal(expectedPoint, builder.Point);
     }
 }
---
+++ src/NOAA.NET.Tests/Zones/Base/ZonesBaseWorkerTests.cs
@@ -0,0 +1,18 @@
+﻿using NOAA.NET.Zones.Base;
+
+namespace NOAA.NET.Tests.Zones.Base;
+
+public class ZonesBaseWorkerTests
+{
+    [Fact]
+    public async void Worker_Working_Constructor()
+    {
+        // Arrange
+        ZonesBaseBuilder builder = null;
+        
+        // Act
+
+
+        // Assert
+    }
+}
